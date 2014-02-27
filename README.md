465eb418aa7e06af23d6379c5044306a66f444ee
Chris Davies <mcd@daviesinc.com>
2014-02-27 02:01:44 +0000

    add glaciergetter and docs

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ aws-glacier-scripts
 
 Set of Glacier tools
 
+* glaciergetter.py - retrieve object from vault
 * glacierls.py - display listing of local storage replica of vault
 * glacierlist.py - request a listing of files in your vault
 * glacierputter.py - upload a file or files from the command line.
---
+++ glaciergetter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import ConfigParser
+import sys
+import os
+
+from boto.glacier.layer2 import Layer2
+
+def main(id):
+    layer2 = Layer2(aws_access_key_id=config.get('glacier',
+         'aws_access_key_id'), aws_secret_access_key=config.get('glacier',
+         'aws_secret_access_key'), region_name=config.get('glacier',
+         'region'))
+    vault = layer2.get_vault(config.get('glacier', 'vault'))
+ 
+    job_id = vault.retrieve_archive(id)
+ 
+    print 'Inventory job id: {0}'.format(job_id)
+
+if __name__ == '__main__':
+    config = ConfigParser.ConfigParser()
+    config.readfp(open(os.path.join('/'.join(sys.argv[0].split('/')[:-1]),
+        'glacierputter.cfg')))
+    
+    try:
+        main(sys.argv[1])
+    except KeyboardInterrupt:
+        sys.exit()
