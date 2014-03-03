7cd318301e3abe3a6af8954c4f70680fae1b2482
Chris Davies <mcd@daviesinc.com>
2014-03-03 01:04:25 +0000

    convert to Layer2, output filename hardcoded

--- glacierresults.py
+++ glacierresults.py
@@ -5,42 +5,45 @@ import json
 import sys
 import os.path
 
-from boto.glacier.layer1 import Layer1
+from boto.glacier.layer2 import Layer2
 from boto.glacier.exceptions import UnexpectedHTTPResponseError
 
-def printjobs(jobs):
-    for job_detail in jobs['JobList']:
-        print 'Request: {0}\nAction: {1}, Status: {2}\n'.format(
-            job_detail['JobId'], job_detail['StatusCode'],
-            job_detail['Action'])
+def printjobs(jobs, header):
+    if jobs:
+        print header
+        for job_detail in jobs:
+            print 'Request: {0}\nAction: {1}, Status: {2}\n'.format(
+                job_detail.id, job_detail.status_code,
+                job_detail.description)
 
 def main(jobid):
-    layer1 = Layer1(aws_access_key_id=config.get('glacier',
+    layer2 = Layer2(aws_access_key_id=config.get('glacier',
          'aws_access_key_id'), aws_secret_access_key=config.get('glacier',
          'aws_secret_access_key'), region_name=config.get('glacier',
          'region'))
-
-    vault = config.get('glacier', 'vault')
+    vault = layer2.get_vault(config.get('glacier', 'vault'))
  
     if jobid:
         try:
-            contents = layer1.get_job_output(vault, jobid)
-            list_of_files = {x['ArchiveId']:x for x in contents['ArchiveList']}
-            contents_file = config.get('glacier','contents')
-            if contents_file:
-                file = open(contents_file, 'w+')
-                file.write(json.dumps(list_of_files))
-                file.close()
+            contents = vault.get_job(jobid)
+            if contents.get('ArchiveList'):
+                list_of_files = {x['ArchiveId']:x for x in contents['ArchiveList']}
+                contents_file = config.get('glacier','contents')
+                if contents_file:
+                    file = open(contents_file, 'w+')
+                    file.write(json.dumps(list_of_files))
+                    file.close()
+            else:
+                if contents.completed:
+                    contents.download_to_file('/tmp/invention.m4v')
         except UnexpectedHTTPResponseError as e:
             print 'ERROR', json.loads(e.body)['message']
             
     else:
-        running_jobs = layer1.list_jobs(vault, completed=False)
-        print 'Running Jobs'
-        printjobs(running_jobs)
-        completed_jobs = layer1.list_jobs(vault, completed=True)
-        print 'Completed Jobs'
-        printjobs(completed_jobs)
+        running_jobs = vault.list_jobs(completed=False)
+        printjobs(running_jobs, 'Running Jobs')
+        completed_jobs = vault.list_jobs(completed=True)
+        printjobs(completed_jobs, 'Completed Jobs')
         
 
 if __name__ == '__main__':
