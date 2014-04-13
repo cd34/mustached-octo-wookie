04996a30005c110b3e91b0b910501a5e1e1e47e3
Chris Davies <mcd@daviesinc.com>
2014-04-13 17:45:00 +0000

    skip existing files in glacier

--- glacierputter.py
+++ glacierputter.py
@@ -25,8 +25,20 @@ def main():
 
     if len(sys.argv) > 1:
         for filename in sys.argv[1:]:
-            if os.path.isfile(filename):
-                archive_description = filename.split('/')[-1]
+            contents_file = config.get('glacier','contents')
+            existing_contents = {}
+            if contents_file:
+                try:
+                    file = open(contents_file, 'r+')
+                    existing_contents = json.loads(file.read())
+                except IOError:
+                    file = open(contents_file, 'w+')
+                file.close()
+            filenames = [x['ArchiveDescription'] for x in \
+                existing_contents.values()]
+            archive_description = filename.split('/')[-1]
+            if os.path.isfile(filename) and archive_description \
+                not in filenames:
                 id = uploader.upload(filename, archive_description)
                 print 'Uploaded: {0}, id: {1}'.format(filename, id) 
                 filesize = 0
@@ -35,26 +47,26 @@ def main():
                 except OSError:
                     pass
                 
-                contents_file = config.get('glacier','contents')
+                #contents_file = config.get('glacier','contents')
                 list_of_files = {id:{'ArchiveId':id, 
                     'ArchiveDescription':archive_description,
                     'CreationDate':datetime.datetime.now(). \
                         strftime('%Y-%m-%dT%H:%M:%SZ'),
                     'Size':filesize}}
                 if contents_file:
-                    existing_contents = {}
-                    try:
-                        file = open(contents_file, 'r+')
-                        existing_contents = json.loads(file.read())
-                    except IOError:
-                        file = open(contents_file, 'w+')
+                    file = open(contents_file, 'w+')
                     list_of_files = dict(list_of_files.items() + \
                         existing_contents.items())
                     file.seek(0)
                     file.write(json.dumps(list_of_files))
                     file.close()
             else:
-                print 'Couldn\'t find file: {0}'.format(filename) 
+                if not os.path.isfile(filename):
+                    print 'Couldn\'t find file: {0}'.format(filename) 
+                else:
+                    print 'File {filename} is already in glacier' \
+                        .format(filename=filename) 
+    
     else:
         print """\
 Need at least one filename on the command line, can accept globs.
