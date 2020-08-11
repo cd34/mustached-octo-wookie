a925cd9cf3492969de288993dc41143f475eb5e3
Chris Davies <c.gitlab@cd34.com>
2020-08-11 17:39:58 +0000

    adding some tests

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ requires = [
     "black",
     "boto",
     "boto3",
+    "pytest",
 ]
 
 if sys.version_info[:3] < (2, 5, 0):
---
+++ tests/libs/test_methods.py
@@ -0,0 +1,34 @@
+import os
+import configparser
+import sys
+
+import libs.methods
+
+config = configparser.ConfigParser()
+config.read_file(
+    open("glacierputter.cfg.sample")
+)
+
+class TestContents:
+
+    def test_existing_contents(self):
+        existing_contents = libs.methods.get_local_contents(config)
+        assert type(existing_contents) == dict
+
+    def test_existing_files(self):
+        existing_contents = libs.methods.get_local_contents(config)
+
+        existing_files = [x["ArchiveDescription"] for x in existing_contents.values()]
+        assert type(existing_files) == list
+
+    def test_file(self):
+        existing_contents = libs.methods.get_local_contents(config)
+        existing_files = [x["ArchiveDescription"] for x in existing_contents.values()]
+
+        assert 'test_data.m4v' in existing_files
+
+    def test_id(self):
+        existing_contents = libs.methods.get_local_contents(config)
+
+        assert existing_contents.get('FeBDpUkqBFnlDRYu4kNMSWVgBUrur-YLbJG6FiWwuV3iephZCClg6U-smty7rgi2hm8rOFTwnFNFOLvoLqW2Tlgb2V1NLzkVxe71m5OcsD9coOGfju6KRzRjN5gCP4s9IOycOcoIPA')
+        assert existing_contents.get('nothere') == None
